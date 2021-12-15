ed22751d2cd5971253cdfff8e09e2264c3fb54eb
Brianna Cerkiewicz <brianna@button.is>
2021-12-15 14:05:15 -0800

    test: pseudocode IDIR-authenticated create opportunity

---
+++ cypress/integration/gov-create-cwu-opportunity.spec.js
@@ -0,0 +1,28 @@
+/// <reference types="cypress" />
+
+describe('As a user authenticated via IDIR', function() {
+    beforeEach(function() {
+      // add fixture data to db
+    })
+
+    it('creates a new published opportunity', function() {
+
+      // visit http://localhost:3000/opportunities/create
+      // click get started on cwu card
+      // fill out form, click next button, repeat for all tabs
+      // save draft
+      // visit dashboard
+      // click on opportunity
+      // confirm all data is present
+      // click actions dropdown
+      // click submit
+      // in modal, check boxes to acknowledge reading terms and conditions
+      //click submit
+      // visit https://localhost:3000/opportunities
+      // click opportunity
+      // confirm all data is present
+    })
+
+
+
+});
