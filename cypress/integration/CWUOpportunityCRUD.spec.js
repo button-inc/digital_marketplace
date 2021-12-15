9905ea3994955931fc5ca58c7aded0005a9728dd
Brianna Cerkiewicz <brianna@button.is>
2021-12-15 11:43:51 -0800

    test: update test

--- cypress/integration/CWUOpportunityCRUD.spec.js
+++ cypress/integration/CWUOpportunityCRUD.spec.js
@@ -1,45 +1,72 @@
 /// <reference types="cypress" />
 
-describe('CRUD for CWU opportunities', function() {
+describe('Creates a new CWU opportunity', function() {
     beforeEach(function() {
       cy.visit('localhost:3000')
     })
 
-    it('creates a new CWU opportunity', function() {
+    // happy path
+    it('authenticates gov user', function() {
+      // logs in as gov user
+
+    })
+
+    it('visits opportunity creation page', function() {
+      // above steps plus:
       // visit http://localhost:3000/opportunities/create
       // click get started on cwu card
+
+    })
+
+    it ('fills out opportunity form', function(){
+      // above steps plus:
       // fill out form, click next button, repeat for all tabs
+
+    })
+
+    it ('saves opportunity as a draft', function(){
+      // above steps plus:
       // save draft
+
+    })
+
+    it ('confirms draft was saved', function(){
+      // above steps plus:
+      // visit dashboard
+      // click on opportunity
+      // confirm all data is present
+
+    })
+
+    it ('publishes opportunity', function(){
+      // above steps plus:
       // click actions dropdown
       // click submit
       // in modal, check boxes to acknowledge reading terms and conditions
       //click submit
-      // test if it saved--click view opportunity link in toast and confirm the opp name is the same as what was entered?
+
     })
 
-    it('reads a CWU opportunity', function() {
-      // visit http://localhost:3000/dashboard
-      // click a cwu opportunity
-      // test if it displays
+
+    it ('confirms opportunity was published', function(){
+      // above steps plus:
+      // visit https://localhost:3000/opportunities
+      // click opportunity
+      // confirm all data is present
+
     })
 
-    it('updates a CWU opportunity', function() {
-      // visit http://localhost:3000/dashboard
-      // click a cwu opportunity
-      // click opportunity tab
-      // click actions and in dropdown, edit
-      // change form fields, click next button, repeat for all tabs
-      // click submit changes
-      // in modal, acknowledge terms
-      // click submit
-      // test if it saved
+    // sad path
+    it('authenticates vendor user', function() {
+      // logs in as vendor user
+
     })
 
-    it('deletes a CWU opportunity', function() {
-      // visit http://localhost:3000/dashboard
-      // click a cwu opportunity
-      // click withdraw
-      // in modal, click withdraw
-      // test if it saved
+    it('cannot access opportunity creation page', function() {
+      // above step plus:
+      // visit http://localhost:3000/opportunities/create
+      // gets page not found message
+
     })
+
 });
