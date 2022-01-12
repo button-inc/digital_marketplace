91c29512fe2bc9ddab7b9ef504e79fc30759a156
Michael Vesprini <mike@button.is>
2022-01-12 10:22:30 -0800

    chore: add TAG param to helm deploy script

--- helm_deploy.sh
+++ helm_deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+set -euxo pipefail
 
 NAMESPACE=$1
 CHART_PATH=$2
+TAG=$3
 echo "Creating Helm installation in $NAMESPACE namespace"
 
-helm upgrade --install --atomic -n $NAMESPACE digital-marketplace $CHART_PATH
+helm upgrade --install --atomic -n $NAMESPACE digital-marketplace $CHART_PATH --set tag=$TAG
